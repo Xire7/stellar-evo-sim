950bd647df83e46ebb1d25e15df3b79be7fe82e7
Zion Mateo <mateoz@uci.edu>
2025-06-06 21:46:38 -0700

    deploy

--- stellar-evo-sim/vite.config.js
+++ stellar-evo-sim/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stellar-evo-sim/', // Replace with your actual repo name
-  build: {
-    outDir: 'dist'
-  }
+  base: '/stellar-evo-sim', // Replace with your actual repo name
 })
